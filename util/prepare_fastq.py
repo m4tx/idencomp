3dc807dc06c9db4d47bbc72f81573b28aea3dfc9
Mateusz Maćkowski <mateusz@mackowski.org>
2022-09-02 22:29:53 +0200

    feat: add --seq-length and implement --chunk in prepare_fastq.py

--- util/prepare_fastq.py
+++ util/prepare_fastq.py
@@ -9,6 +9,9 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     'input', help='input file path')
 parser.add_argument(
+    '--seq-length', type=int, default=0,
+    help='desired length of a single sequence')
+parser.add_argument(
     '--size', type=str,
     help='desired size of the output file in bytes')
 parser.add_argument(
@@ -30,11 +33,14 @@ if args.size is not None:
     val = int(''.join([c for c in args.size if c.isdigit()]))
     max_size = val * mult
 
+seq_len = args.seq_length
+chunk_index = args.chunk
+
 input_path = Path(args.input)
 stem = input_path.stem
 stem += '.noident'
 if args.size is not None:
-    stem += f'.{args.size}.{args.chunk}'
+    stem += f'.{args.seq_length}.{args.size}.{args.chunk}'
 output_path = input_path.with_stem(stem)
 
 lines = []
@@ -43,7 +49,10 @@ output_file = open(output_path, 'w')
 
 print(f'Output file path: {output_path}')
 
+current_chunk_index = 0
 size = 0
+acids_chunk = ''
+q_scores_chunk = ''
 for line in input_file:
     lines.append(line.strip())
     if len(lines) < 4:
@@ -52,14 +61,27 @@ for line in input_file:
     identifier, acids, separator, q_scores = lines
     lines = []
 
+    acids_chunk += acids
+    q_scores_chunk += q_scores
+    if len(acids_chunk) < seq_len:
+        continue
+
     sequence = ''
     sequence += '@\n'
-    sequence += f'{acids}\n'
+    sequence += f'{acids_chunk}\n'
     sequence += f'+\n'
-    sequence += f'{q_scores}\n'
+    sequence += f'{q_scores_chunk}\n'
 
-    output_file.write(sequence)
+    acids_chunk = ''
+    q_scores_chunk = ''
+
+    if current_chunk_index == chunk_index:
+        output_file.write(sequence)
     size += len(sequence)
 
     if size >= max_size:
-        break
+        if current_chunk_index == chunk_index:
+            break
+        else:
+            current_chunk_index += 1
+            size = 0
